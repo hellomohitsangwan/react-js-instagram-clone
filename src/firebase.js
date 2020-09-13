88aaa544562f7d71fd683cf4486c335977b5f13c
hellomohitsangwan <69501650+hellomohitsangwan@users.noreply.github.com>
2020-09-13 14:13:59 +0530

    updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import firebase from "firebase";
 const firebaseApp = firebase.initializeApp(
 
 {
-  apiKey: "AIzaSyDtJDkpr3KPPzZuMyBA3Pc8_dBBegiqA-c",
-  authDomain: "instagram-clone-react-1ff7d.firebaseapp.com",
-  databaseURL: "https://instagram-clone-react-1ff7d.firebaseio.com",
-  projectId: "instagram-clone-react-1ff7d",
-  storageBucket: "instagram-clone-react-1ff7d.appspot.com",
-  messagingSenderId: "161589691092",
-  appId: "1:161589691092:web:c9768418b4bb52a010f141",
-  measurementId: "G-GQ2SF57ZZN"
+  apiKey: "AIzaSyD",
+  authDomain: "instagram-clon",
+  databaseURL: "https://inst",
+  projectId: "instagram-c",
+  storageBucket: "instagra",
+  messagingSenderId: "161",
+  appId: "",
+  measurementId: "G"
 }
 
 );
